5eb25ae1596fc502031a2c082746a45d42f51f5c
BRYSHADOW <ttn21456@gmail.com>
2026-07-12 00:33:59 +0700

    Update URL – 00:33 12/07/2026

--- config.js
+++ config.js
@@ -22,12 +22,12 @@ const CONFIG = {
         instagram: "https://instagram.com",
         website: "https://bory.dev"
     },
-        links: [
+            links: [
         {
             icon: "🎮",
             title: "Delta-VN-By-Bô-Ry",
             description: "Hack Tất Cả Game Trên Mọi Nền Tảng Roblox",
-            url: "https://gofile.io/d/KNO2wS"
+            url: "https://gofile.io/d/VdQzQ6"
         },
         {
             icon: "🌸",
